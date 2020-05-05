db516da4fddc86148cbb4bcf9e3570c31abd8dd7
BuildTools <unconfigured@null.spigotmc.org>
2020-05-05 08:01:00 -0400

    Added files(hope it actually works)

---
+++ lyra_co2.sql
@@ -0,0 +1,30 @@
+USE lyrainsurance
+CREATE TABLE TIMES_USED (
+Nameofuser VARCHAR(50),
+Ageofuser VARCHAR(30),
+Type VARCHAR(20),
+Payment VARCHAR(30),
+Pay_Due_Date VARCHAR(50),
+ID VARCHAR(50),
+`Date` VARCHAR(50));
+
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,Date)
+VALUES (Ponyville,34,injury,2021,2595,5d5m2020y)
+
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,Date)
+VALUES (lemon_the_second,20,Flash_bee_sting,26315$,721919,2020,4d1m1919y)
+
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,Date)
+VALUES (Bon_Bon,NULL,Bug_Bear,NULL,JUST_PAY_ME_ALREAY_BONBON,1038,0000)
+
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,Date)
+VALUES (strawberry_jello,70,Great_Seedling_Foot_Stepping,4d4m2020y)
+
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,Date)
+VALUES (Twilight_Sparkle,45,Parasprite_Attack,852702$,now,5d5m2020y)
+
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,Date)
+VALUES (Ponyname,Ponyage,Ponytype,Ponypayment,Ponypayduedate,PonyID,Ponydate)
+
+
+
