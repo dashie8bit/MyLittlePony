108adc28858b888b70dfc8b5b68cf4baf30cb722
BuildTools <unconfigured@null.spigotmc.org>
2020-05-05 09:16:57 -0400

    Fixed TIMES_USED

--- lyra_co2.sql
+++ lyra_co2.sql
@@ -8,23 +8,21 @@ Pay_Due_Date VARCHAR(50),
 ID VARCHAR(50),
 `Date` VARCHAR(50));
 
-INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,Date)
-VALUES (Ponyville,34,injury,2021,2595,5d5m2020y)
-
-INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,Date)
-VALUES (lemon_the_second,20,Flash_bee_sting,26315$,721919,2020,4d1m1919y)
-
-INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,Date)
-VALUES (Bon_Bon,NULL,Bug_Bear,NULL,JUST_PAY_ME_ALREAY_BONBON,1038,0000)
-
-INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,Date)
-VALUES (strawberry_jello,70,Great_Seedling_Foot_Stepping,4d4m2020y)
-
-INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,Date)
-VALUES (Twilight_Sparkle,45,Parasprite_Attack,852702$,now,5d5m2020y)
-
-INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,Date)
-VALUES (Ponyname,Ponyage,Ponytype,Ponypayment,Ponypayduedate,PonyID,Ponydate)
-
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,`Date`)
+VALUES ('Ponyville','34','injury','2021$','225125','2595','5d5m2020y');
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,`Date`)
+VALUES ('lemon_the_second','20','Flash_bee_sting','26315$','721919','2020','4d1m1919y');
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,`Date`)
+VALUES ('Bon_Bon','NULL','Bug_Bear','NULL','JUST_PAY_ME_ALREAY_BONBON','1038','0000');
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,`Date`)
+VALUES ('strawberry_jello','70','Great_Seedling_Foot_Stepping','632334$','225253','2503','4d4m2020y');
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,`Date`)
+VALUES ('Twilight_Sparkle','45','Parasprite_Attack','852702$','now','Wings','5d5m2020y');
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,`Date`)
+VALUES ('Ponyname','Ponyage','Ponytype','Ponypayment','Ponypayduedate','PonyID','Ponydate');
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment, Pay_Due_Date,ID,`Date`)
+VALUES ('Fern','24','Timberwolf_Happened','235209$','7532','302058','3d1m0000y');
+INSERT INTO TIMES_USED(Nameofuser, Ageofuser, Type, Payment,Pay_Due_Date,ID,`Date`)
+VALUES ('SocialMedia','NULL','Twittermites','6532','852083','8235','2d3m9342y');
 
 
