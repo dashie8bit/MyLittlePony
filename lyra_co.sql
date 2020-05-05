b81733a2dfe7929274fb593acb950db575225605
BuildTools <unconfigured@null.spigotmc.org>
2020-05-05 07:19:35 -0400

    Yay it works now!

--- lyra_co.sql
+++ lyra_co.sql
@@ -1,20 +1,33 @@
 CREATE DATABASE lyrainsurance;
 use lyrainsurance;
-CREATE TABLE USERS(
-Name VARCHAR(50)
-Age VARCHAR(50)
-ID VARCHAR(50)
-Last_usage(50);
-INSERT INTO USERS(lemon_the_first,1000,1,0s);
-INSERT INTO USERS(Bon_Bon,NULL,1038,29s35m26d);
-INSERT INTO USERS(NULL,NULL,NULL?,NULL);
-INSERT INTO USERS(Sweetie_Belle,NULL,-1023,NULL);
-INSERT INTO USERS(Berry_Punch,21,6357,30s);
-INSERT INTO USERS(Melody,53,2669,26s53m120d10M);
-INSERT INTO USERS(Chrysalis,ITISNONEOFYOURCONCERN,9153,NEITHERISTHAT);
-INSERT INTO USERS(Ponyville,34,2595,1s);
-INSERT INTO USERS(A_Melon,27,0835,melon);
-INSERT INTO USERS(thealphebet,abc,def,ghi);
-INSERT INTO USERS(normalpony,anormalage,anormalid,anormalusage);
-INSERT INTO USERS(Bubbles,ummmidunno,8888,0s)
+CREATE TABLE `USERS`(
+name VARCHAR(80), 
+age VARCHAR(80), 
+id VARCHAR(80), 
+lastuse VARCHAR(80));
+
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('lemon_the_first', '1000', '1', '0s');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('Bon_Bon', 'NULL', '1038', '29s35m26d');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('NULL', 'NULL', 'NULL?', 'NULL');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('Sweetie_Belle', 'NULL', '-1023', 'NULL');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('Berry_Punch', '21', '6357', '30s');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('Melody', '53', '2669', '26s53m120d10M');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('Chrysalis', 'ITISNONEOFYOURCONCERN', '9153', 'NEITHERISTHAT');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('Ponyville', '34', '2595', '1s');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('A_Melon', '27', '0835', 'melon');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('thealphebet', 'abc', 'def', 'ghi');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('normalpony', 'anormalage', 'anormalid', 'anormalusage');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('Bubbles', 'ummmidunno', '8888', '0s');
 
