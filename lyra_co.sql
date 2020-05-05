5b513da7255b89f5fe02ab479a3bfc5c2bca7154
BuildTools <unconfigured@null.spigotmc.org>
2020-05-05 10:38:13 -0400

    Added more ponies to user database

--- lyra_co.sql
+++ lyra_co.sql
@@ -9,7 +9,7 @@ lastuse VARCHAR(80));
 INSERT INTO USERS(name,age,id,lastuse)
 VALUES ('lemon_the_first', '1000', '1', '0s');
 INSERT INTO USERS(name,age,id,lastuse)
-VALUES ('Bon_Bon', 'NULL', '1038', '29s35m26d');
+VALUES ('Bon_Bon', 'NULL', '1038', '293526d');
 INSERT INTO USERS(name,age,id,lastuse)
 VALUES ('NULL', 'NULL', 'NULL?', 'NULL');
 INSERT INTO USERS(name,age,id,lastuse)
@@ -17,7 +17,7 @@ VALUES ('Sweetie_Belle', 'NULL', '-1023', 'NULL');
 INSERT INTO USERS(name,age,id,lastuse)
 VALUES ('Berry_Punch', '21', '6357', '30s');
 INSERT INTO USERS(name,age,id,lastuse)
-VALUES ('Melody', '53', '2669', '26s53m120d10M');
+VALUES ('Melody', '53', '2669', '265312010');
 INSERT INTO USERS(name,age,id,lastuse)
 VALUES ('Chrysalis', 'ITISNONEOFYOURCONCERN', '9153', 'NEITHERISTHAT');
 INSERT INTO USERS(name,age,id,lastuse)
@@ -30,4 +30,19 @@ INSERT INTO USERS(name,age,id,lastuse)
 VALUES ('normalpony', 'anormalage', 'anormalid', 'anormalusage');
 INSERT INTO USERS(name,age,id,lastuse)
 VALUES ('Bubbles', 'ummmidunno', '8888', '0s');
-
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('Luna','THOU SHALL NO KNOW THY AGE','0005','30s');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('Berry Sweets','22','0026','50s');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('lemon_the_maybe','519023481023431','0512','1500y');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('citrus','20','8263','NULL');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('lemon dreams','30','6325','362638y');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES ('melon melody','26','2502','6234352y');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES('kiwi','25','2635','272935y');
+INSERT INTO USERS(name,age,id,lastuse)
+VALUES('sql injection','1998','1852','151963y');
